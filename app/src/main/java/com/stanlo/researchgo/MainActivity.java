95976c44309c38d94599693e36c0e75f83b1e3de
stanlo-icnhs <ktanzievlogs@gmail.com>
2019-10-10 19:59:05 +0800

    Preparation for OneUI Styled cardview

--- app/src/main/java/com/stanlo/researchgo/MainActivity.java
+++ app/src/main/java/com/stanlo/researchgo/MainActivity.java
@@ -6,9 +6,7 @@ import androidx.annotation.StyleRes;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import androidx.cardview.widget.CardView;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
 
 import android.view.View;
 import android.widget.AdapterView;
@@ -19,18 +17,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ListView list;
 
-    String[] maintitle ={
-            "Research Paper Making","Quizzes",
-            "Sample Research Paper","FAQ",
-    };
-
-
-    Integer[] imgid={
-            R.drawable.ic_research_paper,R.drawable.ic_gamepad,
-            R.drawable.ic_sample_paper,R.drawable.faq,
-    };
 
 
 
@@ -45,42 +32,10 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
 
-        //contents
-        MyListAdapter adapter=new MyListAdapter(this, maintitle,imgid);
-        list= findViewById(R.id.list);
-        list.setAdapter(adapter);
-
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
-                // TODO Auto-generated method stub
-                if(position == 0) {
-                    //code specific to first list item
-                    Toast.makeText(getApplicationContext(),"Place Your First Option Code",Toast.LENGTH_SHORT).show();
-                }
-
-                else if(position == 1) {
-                    //code specific to 2nd list item
-                    Toast.makeText(getApplicationContext(),"Place Your Second Option Code",Toast.LENGTH_SHORT).show();
-                }
-
-                else if(position == 2) {
-
-                    Toast.makeText(getApplicationContext(),"Place Your Third Option Code",Toast.LENGTH_SHORT).show();
-                }
-                else if(position == 3) {
-
-                    Toast.makeText(getApplicationContext(),"Place Your Forth Option Code",Toast.LENGTH_SHORT).show();
-                }
-                else if(position == 4) {
-
-                    Toast.makeText(getApplicationContext(),"Place Your Fifth Option Code", Toast.LENGTH_SHORT).show();
-                }
-
-            }
 
 
-    });
+
+    }
 
 
 
@@ -92,5 +47,5 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-}}
+}
 
---
+++ app/src/main/java/com/stanlo/researchgo/settings_page.java
@@ -0,0 +1,14 @@
+package com.stanlo.researchgo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class settings_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_settings_page );
+    }
+}
